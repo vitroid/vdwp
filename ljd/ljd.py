2475395b03bb0e8e37856011fc4c77ee4ebe72ab
vitroid <vitroid@gmail.com>
2025-09-08 02:44:39 +0900

    Refactor cellpotential and fvalue functions for improved readability and type hinting; deprecate old fvalue function.

--- ljd/ljd.py
+++ ljd/ljd.py
@@ -1,6 +1,10 @@
 import numpy as np
+from vdwp.decorators import deprecated
 
-def cellpotential(r, sigma, epsilon, z, R, a=0.0):
+
+def cellpotential(
+    r: np.ndarray, sigma: float, epsilon: float, z: int, R: float, a: float = 0.0
+) -> np.ndarray:
     """
     Lennard-Jones--Devonshire cellpotential function.
 
@@ -8,26 +12,75 @@ def cellpotential(r, sigma, epsilon, z, R, a=0.0):
     R: free cavity radius; 3.908, 4.326 AA for 12- and 14-hedral cages, respectively.
     r: position of the guest from the center.
     """
-    def delta(N):
-        return ((1 - r / R - a / R)**(-N) - (1 + r / R - a / R)**(-N)) / N
 
-    ans = 2 * z * epsilon * (sigma**12 / (R**11 * r) * (delta(10) + a * delta(
-        11) / R) - sigma**6 / (R**5 * r) * (delta(4) + a * delta(5) / R))
+    def delta(N):
+        return ((1 - r / R - a / R) ** (-N) - (1 + r / R - a / R) ** (-N)) / N
+
+    ans = (
+        2
+        * z
+        * epsilon
+        * (
+            sigma**12 / (R**11 * r) * (delta(10) + a * delta(11) / R)
+            - sigma**6 / (R**5 * r) * (delta(4) + a * delta(5) / R)
+        )
+    )
     return ans
 
-def fvalue(Rz, sigma, epsilon, beta, plt=None):
-    r = np.linspace(0,max(Rz.keys()),100)
+
+def fvalue2(R: float, z: int, sigma: float, epsilon: float, beta: float) -> float:
+    """
+    Lennard-Jones--Devonshire cellpotential function.
+
+    R: radius
+    z: number of water molecules constituting the cavity
+    sigma: sigma
+    epsilon: epsilon
+    beta: beta
+    """
+    # 0とmaxの間に98点を追加して100点の配列を作る
+    r = np.linspace(0, R, 100)
+    dr = r[1] - r[0]
+    cp = np.zeros(100)
+    r1 = r[r < R]
+    r1 = r1[1:]
+    cp1 = cellpotential(r1, sigma, epsilon, z, R)
+    cp[1 : len(cp1) + 1] += cp1
+    return (
+        -np.log(4 * np.pi * np.trapezoid(np.exp(-beta * cp) * r**2 * 1e-30, dx=dr))
+        / beta
+    )
+
+
+# test
+@deprecated("fvalue2")
+def fvalue(Rz: dict, sigma: float, epsilon: float, beta: float, plt=None) -> float:
+    """
+    Lennard-Jones--Devonshire cellpotential function.
+
+    Rz: dict of {R: z}; R is the radius and z is the number of water molecules constituting the cavity.
+    sigma: sigma
+    epsilon: epsilon
+    beta: beta
+    """
+    # 0とmaxの間に98点を追加して100点の配列を作る
+    r = np.linspace(0, max(Rz.keys()), 100)
+    dr = r[1] - r[0]
     cp = np.zeros(100)
     for R, z in Rz.items():
-        r1 = r[r<R]
+        r1 = r[r < R]
+        length = len(r1)
         r1 = r1[1:]
         cp1 = cellpotential(r1, sigma, epsilon, z, R)
-        cp[:cp1.shape[0]] += cp1
+        cp[1 : length + 1] += cp1
     if plt is not None:
-        plt.plot(r, cp*beta)
+        plt.plot(r, cp * beta)
     # plt.plot(r, np.exp(-cellpotential(r, sigma, epsilon, z, R) * beta)*r**2)
     # print(z, np.trapz(np.exp(-beta * cellpotential(r, sigma, epsilon, z, R)) * r**2, dx=r[1]-r[0]))
-    return -np.log(4 * np.pi * np.trapz(np.exp(-beta * cp) * r**2 * 1e-30, dx=r[1]-r[0])) / beta
+    return (
+        -np.log(4 * np.pi * np.trapezoid(np.exp(-beta * cp) * r**2 * 1e-30, dx=dr))
+        / beta
+    )
 
 
 # 2022-03-25とりあえず書いただけ。まだ使っていない。
